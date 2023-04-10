11211121e9145df7716b362201c960aac64fa103
rooprete1cham <125527744+rooprete1cham@users.noreply.github.com>
2023-04-10 20:27:24 +0900

    Add files via upload

---
+++ C언어 도전 150제/1. 변수형 개념 배우기/1. 변수형 개념 배우기/maincpp.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x = 1;        // 변수 x에 1 저장
+
+    int y = 2;        // 변수 y에 2 저장
+
+    int z = x + y;    // x와 y의 합을 z에 저장
+
+    printf("x: %d\n", x);
+    printf("y: %d\n", y);
+    printf("z: %d\n", z);
+
+    return 0;
+}
---
+++ C언어 도전 150제/3. 연산자 개념 배우기/3. 연산자 개념 배우기/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 10;  //변수 x에 10대입
+
+	x = x - 5;   //x에서 5를 뺌
+
+	printf("x에서 5를 뺀 결과: %d\n", x);
+
+	return 0;
+
+}
